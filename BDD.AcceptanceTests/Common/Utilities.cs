94ae53b6bca5131d35fffa928c6fedcab3cdf479
Michal Boczula <michal.boczula0101@gmail.com>
2023-12-31 20:29:52 +0100

    Added Test

---
+++ BDD.AcceptanceTests/Common/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD.AcceptanceTests.Common
+{
+    public class Utilities
+    {
+        public static async Task<T> GetResponseContent<T>(HttpResponseMessage response)
+        {
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<T>(stringResponse);
+            return result;
+        }
+    }
+}
